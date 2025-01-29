6cdf816f18b1c41f8e5c8ab2296ff17afdefa006
shovan-642 <shovan642@gmail.com>
2025-01-30 00:43:05 +0600

    navbar connected

---
+++ src/components/root/Root.jsx
@@ -0,0 +1,13 @@
+import Home from "../Home/Home";
+
+
+
+const Root = () => {
+    return (
+        <div>
+            <Home></Home>>
+        </div>
+    );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter } from 'react-router'
+import Root from './components/root/Root.jsx'
+import { RouterProvider } from 'react-router/dom'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <RouterProvider router={router}/>
+</StrictMode>,
 )
